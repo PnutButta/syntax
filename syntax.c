26d6376a7398a10649d466d15a83239d2e7f1ff0
Angel Mitchell <angel.mitchell@bison.howard.edu>
2017-03-23 15:23:03 -0400

    endless loop error fixed

--- syntax.c
+++ syntax.c
@@ -59,7 +59,7 @@ int main() {
             getChar(); do {
                 lex();
                 expr();
-            } while ((nextToken != EOF) || (nextChar != '\n'));
+            } while ((nextToken != EOF) || (nextChar == '\n'));
         }
 }
 
